a15569040440e066dcdb8f26c31427b45bd61748
Samee28 <raisameeksha174@gmail.com>
2025-12-04 18:28:28 +0530

    Added RouteService logic

--- backend/src/core/application/routeService.ts
+++ backend/src/core/application/routeService.ts
@@ -1,26 +1,17 @@
-import { Route } from '../domain/route';
-import { RoutePort } from '../ports/routePort';
+import { RoutePort } from "../ports/routePort";
 
 export class RouteService {
-  constructor(private routeRepository: RoutePort) {}
+  constructor(private routePort: RoutePort) {}
 
-  async getAllRoutes(): Promise<Route[]> {
-    return this.routeRepository.findAll();
+  async listRoutes() {
+    return this.routePort.getAllRoutes();
   }
 
-  async getRouteById(id: number): Promise<Route | null> {
-    return this.routeRepository.findById(id);
+  async setBaselineRoute(routeId: string) {
+    return this.routePort.setBaseline(routeId);
   }
 
-  async createRoute(route: Omit<Route, 'id'>): Promise<Route> {
-    return this.routeRepository.create(route);
-  }
-
-  async updateRoute(id: number, route: Partial<Route>): Promise<Route | null> {
-    return this.routeRepository.update(id, route);
-  }
-
-  async deleteRoute(id: number): Promise<boolean> {
-    return this.routeRepository.delete(id);
+  async getComparison() {
+    return this.routePort.getComparisonRoutes();
   }
 }
